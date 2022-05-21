9dbf8efc06873f4d5ce8ee342e0d8009e60b0a88
tarazani <starazani5692@gmail.com>
2022-05-21 16:59:33 +0430

    بهینه سازی ارتباط کلاینت با سرور هنگام لاگین

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -14,7 +14,7 @@ function sha1(txt) {
 
 const controller = {
     login: (req, res) => {
-        console.log("**********************************");
+        console.log("Login**********************************");
         console.log(req.body);
         outputCode = 0;
         if(req.body.user){
@@ -43,44 +43,49 @@ const controller = {
                                     const userFullName = iconv.decode(result[0].USRFULLNAME, 'WINDOWS-1256');
                                     const userDes = iconv.decode(result[0].USRDESCRIPTION, 'WINDOWS-1256');
                                     const accessToken = jwt.sign({userId, userName, userFullName, userDes}, process.env.ACCESS_TOKEN_SECRET,{
-                                        expiresIn: '15s'
+                                        expiresIn: '60m'
                                     });
                                     const refreshToken = jwt.sign({userId, userName, userFullName, userDes}, process.env.REFRESH_TOKEN_SECRET,{
                                         expiresIn: '1d'
                                     });
-                                    console.log(accessToken);
+                                    console.log("////////////" + accessToken);
                                     // console.log("*********************************************************************");
                                     // console.log(refreshToken);
                                     db.query('update TPUB_USERS set REFRESH_TOKEN = ? where USR_UID = ?',
                                         [accessToken, userId],
                                         function(err, result) {
-                                            console.log(result);
+                                            console.log("Result:" + result);
+                                            db.detach();
+                                            res.status(200).json({accessToken});
                                             db.detach();
                                         });
-                                    res.cookie('refreshToken', refreshToken,{
-                                        httpOnly: true,
-                                        maxAge: 24 * 60 * 60 * 1000
-                                    });
-                                    res.status(200).json({accessToken});
+                                       
+                                    // res.cookie('refreshToken', refreshToken,{
+                                    //     httpOnly: true,
+                                    //     maxAge: 24 * 60 * 60 * 1000
+                                    // });
+                                    
+                                    
                                 } else res.status(404).json({msg: 'کاربری شما غیر فعال می باشد .'});
                             } else res.status(404).json({msg : "نام کاربری یا رمز عبور شما اشتباه می باشد ."});
                         }
                     );
-                    db.detach();
                 });
+                Firebird
             } else {
                 res.status(404).json({msg : 'Note Password'});
             }
         }
-        var pass = sha1('123');
     },
     token: (req, res) => {
+        console.log("Token*************************************");
         if(req.body.accessToken) {
-            console.log(req.body.accessToken);
+            console.log("req.token: " + req.body.accessToken);
             jwt.verify(req.body.accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
                 if(err) return res.sendStatus(401);
-                req.userId = decoded.userId;
-                Firebird.attach(fboption, (er, db ) => {
+                console.log("jwtverify: " + decoded.userId);
+                // req.userId = decoded.userId;
+                Firebird.attach(fboption, function (err, db) {
                     if (err) return res.sendStatus(402);
                     console.log("Connection successfully...");
                     db.query('select REFRESH_TOKEN from TPUB_USERS where USR_UID = ?', [decoded.userId], (err, result) => {
@@ -88,11 +93,12 @@ const controller = {
                             console.log(result[0].REFRESH_TOKEN);
                             if (req.body.accessToken === result[0].REFRESH_TOKEN) {
                                 console.log("OK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                                res.status(200).json(result[0].REFRESH_TOKEN);
+                                res.status(200).json({accessToken: result[0].REFRESH_TOKEN});
                             } else return res.sendStatus(403);
                         } else return res.sendStatus(404);
-                    })
-                })
+                        db.detach();
+                    });
+                });
             });
         }
     }
